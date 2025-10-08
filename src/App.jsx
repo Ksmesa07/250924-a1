caf08b9ef29c7314bced6ec8f492870f2c267424
Ksmesa07 <116425636+Ksmesa07@users.noreply.github.com>
2025-10-08 15:09:11 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,12 @@ import Outlet from "./components/layout/Outlet"
 
 export default function App() {
   return (
-    <>
-      <div className='min-h-screen flex flex-col bg-base-100'>
-        <Heater/>
-        <main className="flex-1 container mx auto px-4 py-8">
-          <Outlet/>
-        </main>
-        <Footer/>
-      </div>
-    </>
+    <div className='min-h-screen flex flex-col bg-base-100'>
+      <Heater/>
+      <main className="flex-1 container mx auto px-4 py-8">
+        <Outlet/>
+      </main>
+      <Footer/>
+    </div>
   )
 }
