7e5a1baa916a99e0722702c251f71be79d0f2892
Ksmesa07 <116425636+Ksmesa07@users.noreply.github.com>
2025-10-08 14:40:58 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import {createBrowserRouter,RouterProvider} from "react-router-dom"
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Home from './page/Home.jsx'
+import About from './page/About.jsx'
+import Contact from './page/Contact.jsx'
+import Menu from './page/Menu.jsx'
+
+const router=createBrowserRouter([
+  {
+    path:"/",
+    element:<App/>,
+    Children:[
+      {index:true,element:<Home/>},
+      {path:"menu",element:<Menu/>},
+      {path:"about",element:<About/>},
+      {path:"contact",element:<Contact/>},
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
