e0823622293530d53fc797b17db00a21316ccb6a
Ksmesa07 <116425636+Ksmesa07@users.noreply.github.com>
2025-10-22 14:43:52 +0800

    Update Home.jsx

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,3 +1,9 @@
+import React from "react"
+import { link } from "react-router-dom"
+
 export default function Home(){
+    const features =[
+        {icon: "⚡", label: "Fast Build Times"},
+    ]
     return <h1>Home</h1>
 }
