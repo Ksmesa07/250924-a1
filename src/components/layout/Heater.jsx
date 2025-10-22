bb5d4e23a40e258a1f2f3ff9c320753bda6132c8
Ksmesa07 <116425636+Ksmesa07@users.noreply.github.com>
2025-10-22 18:02:15 +0800

    1022

--- src/components/layout/Heater.jsx
+++ src/components/layout/Heater.jsx
@@ -1,14 +1,16 @@
 import React,{useState} from "react"
-import { link,useLocation} from "react-router-dom"
+import { Link,useLocation} from "react-router-dom"
 import NavLinks from "./NavLinks"
 import { NavLink,isPathActive } from "react-router-dom" 
-import Link from "daisyui/components/link";
-
+import {HiBar3,HiSun,HiMoon} from "react-icons/hi2";
+import {Hisave} from "react-icons/hi";
+import useTheme from "../../hooks/useTheme";
+import UserMenu from "./UserMenu";
 
 export default function Heater(){
     const [isMenuOpen,setIsMenuOpen]=useState(false);
     const location=useLocation();
-
+    const {theme,setTheme,toggleTheme} =useTheme("light");
     const isNavItemActive=(path)=>isPathActive(path,location.pathname);
     return(
         <header className="navbar bg-base-100 shadow-lg">
@@ -23,7 +25,17 @@ export default function Heater(){
                 <div className="navbar-center hidden md:flex">
                     <NavLinks items={NAV_ITEMS} isActive={isNavItemActive} listClassName={"menu menu-horizontal px-1"}/>
                 </div>
+                <div className="navbar-end flex gap-2 items-center">
+                    <button className="btn btn-ghost btn-circle border-2 bg-base-300" onClick={toggleTheme} aria-label="切換主題">
+                        {theme === "dark" ? (
+                            <HiMoon className="w-6 h-6"/>
+                        ):(
+                            <HiSun className="w-6 h-6"/>
+                        )}
+                    </button>
+                    <UserMenu/>
+                </div>
             </div>
         </header>
     );
-}
+};
